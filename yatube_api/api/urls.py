f3cbf67acb65af51864edee09ac747a04b7d4f17
Andrew <zxasdqwezxasd@mail.ru>
2026-06-03 00:36:48 +0500

    523235

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -15,5 +15,6 @@ router.register(
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('jwt/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
